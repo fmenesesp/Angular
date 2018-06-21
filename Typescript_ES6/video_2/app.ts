af51b150adb0a7d7d2cbc5e250f63a69d1e0b583
Fernando <fernando.meneses@adsalsa.com>
2018-06-21 09:48:35 +0200

    sync

---
+++ Typescript_ES6/video_2/app.ts
@@ -0,0 +1,16 @@
+let mensaje = "hola";
+
+if(true){
+    //Valor solo valido dentro del IF
+    let mensaje = "adios";
+}
+
+console.log(mensaje);
+
+//Solo lectura
+const OPCIONES = "todas";
+
+if(true){
+    //Valor solo valido dentro del IF
+    const OPCIONES = "ninguna";
+}
